9ffb94a6ba97866c6bbaa43f1a9d64625024061b
James Roaden <roaden@gmail.com>
2018-01-10 11:35:23 -0500

    Class stuff.

---
+++ userinput.py
@@ -0,0 +1,13 @@
+#This is a comment.  It will not run when the program runs.
+
+print('Sometimes you want to get input from a user.')
+
+print('Luckily, there is a function for that!')
+
+print("So, what's your name?")
+
+name = input()
+
+print('So, your name is ' + name + '?')
+
+print('Sounds weird.')
