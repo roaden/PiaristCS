3f7f276eedea62e5afbecf7a4e1cd71e1bee4fa1
James Roaden <roaden@gmail.com>
2018-01-23 13:39:09 -0500

    Function examples.

---
+++ week3/classroom.py
@@ -0,0 +1,19 @@
+def brett():
+    print("The kid that has the worst memes.")
+
+def logan():
+    print("Came to school late today.")
+
+def jack():
+    print("HAS THE MOST ANNOYING VOICE.")
+
+def lynzie():
+    print("Oh God, it's Lynzie. Again.  Why?")
+
+
+def kaytlyn():
+    print("Has terrible choice in friends.")
+
+def emmalee():
+    print("She's not here?  Didn't notice.")
+
---
+++ week3/example.py
@@ -0,0 +1,12 @@
+from classroom import *
+
+jack()
+logan()
+brett()
+lynzie()
+kaytlyn()
+emmalee()
+
+import math
+
+print(math.sin(math.pi))
