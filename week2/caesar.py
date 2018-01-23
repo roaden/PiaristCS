aa01fd96a5dfbb71f5adaa7815034ee803816909
James Roaden <roaden@gmail.com>
2018-01-23 11:31:31 -0500

    Added comments to caesar cypher.

--- week2/caesar.py
+++ week2/caesar.py
@@ -1,4 +1,6 @@
+#This function will shift forward by whatever number
 def shift(num, tobeshifted):
+    #Adding in the alphabets that I'm using.
     lower = 'abcdefghijklmnopqrstuvwxyz'
     upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     aftershift = ''
@@ -6,24 +8,24 @@ def shift(num, tobeshifted):
         didshift = False
         for j in range(len(lower)):
             if lower[j] == tobeshifted[i]:
-                aftershift += lower[(j+num)%26]
+                aftershift += lower[(j+num)%26] #If it's there, shift forward by num
                 didshift = True
-                print("I did a thing.")
+                #print("I did a thing.") #This was used in debugging
                 break
-            elif upper[j] == tobeshifted[i]:
-                aftershift += upper[(j+num)%26]
+            elif upper[j] == tobeshifted[i]: #We're assuming uppercase == lowercase
+                aftershift += upper[(j+num)%26] #Same deal here. Shift forward by num
                 didshift = True
-                print("I did another thing")
+                #print("I did another thing") #This was used in debugging.
                 break
-        if not didshift:
+        if not didshift: #If not found, need to add original thing back.
             aftershift += tobeshifted[i]
     return aftershift
 
-theshift = 13
+theshift = 13 #Shifting by 13 for nice easy cypher
 print('Type quit to exit')
 
 while(True):
     thingtoshift = input()
-    if thingtoshift == 'quit':
+    if thingtoshift == 'quit': #Exit if you see quit
         break
-    print(shift(theshift,thingtoshift))
+    print(shift(theshift,thingtoshift)) #Print the shifted value.
