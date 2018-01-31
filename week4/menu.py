7d17f057319a302220d3604e333bf566664120b3
James Roaden <roaden@gmail.com>
2018-01-31 10:07:04 -0500

    Fixed some weird character issues maybe?

--- week4/menu.py
+++ week4/menu.py
@@ -1,9 +1,9 @@
 #We're going to implement scripts from the elementary list on https://adriann.github.io/programming_problems.html
-#    1 Print ‘Hello World’ to the screen.
+#    1 Print 'Hello World' to the screen.
 #    2 Ask the user for their name and greet them with their name.
 #    3 Ask the user for a number n and print the sum of the numbers 1 to n
 #    4 The previous program such that only multiples of three or five are considered in the sum, e.g. 3, 5, 6, 9, 10, 12, 15 for n=17
-#    5 Ask the user for a number n and give them the possibility to choose between computing the sum and computing the product of 1,…,n.
+#    5 Ask the user for a number n and give them the possibility to choose between computing the sum and computing the product of 1,..,n.
 #    6 Write a program that prints a multiplication table for numbers up to 12.
 #    7 Write a guessing game where the user has to guess a secret number. After every guess the program tells the user whether their number was too large or too small. At the end the number of tries needed should be printed. I counts only as one try if they input the same number multiple times consecutively.
 
